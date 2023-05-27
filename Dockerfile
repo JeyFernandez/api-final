c73285a7059b50eb224e1e9bf70991ca199052db
Jasson Fernandez <jeicobfer@gmail.com>
2023-05-28 00:47:39 +0200

    configurando para no levantar con Docker

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM node
-COPY . /api
-WORKDIR /api
-RUN npm install
-CMD ["npm", "run", "start:dev"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { ProductsModule } from './products/products.module';
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      autoLoadEntities: true,
+      autoLoadEntities: true, 
       synchronize: true,
     }),
     ProductsModule
